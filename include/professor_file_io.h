cc7c4f02e0ca18108a16954832760a8871d0441e
hyeonu <hyeonu9831@gmail.com>
2025-06-22 04:03:10 +0900

    cursor

--- include/professor_file_io.h
+++
@@ -1,9 +0,0 @@
-#ifndef PROFESSOR_FILE_IO_H
-#define PROFESSOR_FILE_IO_H
-
-void setup_professor_profile(const char* professor_id);
-void trim_whitespace(char* str);
-void add_subject();
-void delete_subject();
-
-#endif // PROFESSOR_FILE_IO_H
--- include/timestamp.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <time.h>
-
-void get_current_timestamp(char* buffer, size_t size) {
-    time_t now = time(NULL);
-    struct tm* tm_info = localtime(&now);
-    strftime(buffer, size, "%Y-%m-%d_%H:%M:%S", tm_info);
-}
